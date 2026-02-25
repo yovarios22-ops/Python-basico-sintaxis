a6461a0004edd39e924411b2814c11af0bca4416
yovarios22-ops <yovarios22@gmail.com>
2026-02-25 12:23:03 -0500

    first commit

---
+++ 1-tipos-de-datos.py
@@ -0,0 +1,21 @@
+#tipos de datos en python
+#yovani
+#1. tipo de dato numerico (int)
+edad = 25 
+promedio = 19
+numero = -50
+
+#2. tipo de dato decimal (float)
+peso =75.50
+estatura =170.30
+temperatura = -14.70 
+
+#tipo de dato texto (str)
+letra = 'A'
+nombre = 'diego'
+apellido ="lipa"
+dni ='60179568'
+
+#tipo de dato booleanos (bol)
+es_mayor_de_edad = True
+puede_volar = False 
---
+++ 2-operadores.py
@@ -0,0 +1,44 @@
+#operadores en python 
+#yovani
+#1. operadores aritmeticos 
+suma = 5 + 5
+resta = 10-6
+multiplicacion = 5 * 5
+division = 10 / 2
+modulo = 10 % 2 
+exponente = 10 ** 2
+
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(modulo)
+print(exponente)
+
+#2. operadores de comparacion 
+print(5 ==5 )    # igual a 
+print(5 != 5)    #diferente de 
+print(10 > 5 )   #mayor que
+print(10 < 5 )   #menor que 
+print(10 >= 5 )  #mayor igual que
+print(10 <= 5 )  #menor igual que
+
+#3. operadores logicos 
+v = True
+f = False
+#3.1 and (y)
+print('-------------------------AND')
+print(v and v)
+print(v and f)
+print(f and v)
+print(f and f)
+#3.2 or (o)
+print(v or v)
+print(v or  f)
+print(f or v)
+print(f or f)
+#3.3 not (negacion)
+print('-------------------------NOT')
+print(not f)
+print(not v)
